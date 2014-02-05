d94e81baab9a64e5c22fc9c3321a54e1d21ef4e5
Reem Khamis <khamisr@incubaid.com>
2014-02-05 18:40:02 +0200

    dhcp interface fix

--- JumpScale/lib/dhcp/dhcp.py
+++ JumpScale/lib/dhcp/dhcp.py
@@ -1,4 +1,5 @@
 from JumpScale import j
+import netifaces
 import JumpScale.baselib.remote
 
 class DhcpFactory(object):
@@ -14,16 +15,23 @@ class DHCP(object):
         j.remote.cuisine.fabric.env['password'] = password
         self.remoteApi.connect(host)
 
-    def configure(self, ipFrom, ipTo):
-        config = '''default-lease-time 600;
+    def configure(self, ipFrom, ipTo, interface):
+        interface = netifaces.ifaddresses(interface)[2]
+        if j.remote.cuisine.api.file_exists(self.configPath):
+            header = '''default-lease-time 600;
 max-lease-time 7200;
-option subnet-mask 255.255.255.0;
+'''
+            self.remoteApi.run('touch %s' % self.configPath)
+            self.remoteApi.file_write(self.configPath, header)
 
-subnet 192.168.1.0 netmask 255.255.255.0 {
-range %s %s;
-}''' % (ipFrom, ipTo)
-        self.remoteApi.run('touch %s' % self.configPath)
-        self.remoteApi.file_write(self.configPath, config)
+        config = '''
+subnet %s netmask %s {
+    option subnet-mask 255.255.255.0;
+    option routers 10.0.0.1;
+    range %s %s;
+}''' % (interface['addr'], interface['netmask'], ipFrom, ipTo)
+
+        self.remoteApi.file_append(self.configPath, config)
         self.restart()
 
     def start(self):
