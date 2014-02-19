19921cedb75e80c4af6b55fd358402ecabc3616d
kristof <kristof@incubaid.com>
2014-02-19 06:08:18 +0100

    ?

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+
+
+setup(name='JumpScale Libs',
+      version='6.0.0',
+      description='Python Automation framework',
+      author='JumpScale',
+      author_email='info@jumpscale.org',
+      url='http://www.jumpscale.org',
+
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe = False,
+      namespace_packages = ['JumpScale'],
+
+      download_url='http://pypi.python.org/pypi/JumpScale/',
+      install_requires=[],
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
+
