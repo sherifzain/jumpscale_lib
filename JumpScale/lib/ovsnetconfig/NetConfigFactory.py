2f3ffa1cf80c6f0064fe6896fa1413f4329edc1f
kristof <kristof@incubaid.com>
2014-03-18 16:36:10 +0100

    networkconfig tools for ovs

--- JumpScale/lib/ovsnetconfig/NetConfigFactory.py
+++ JumpScale/lib/ovsnetconfig/NetConfigFactory.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from JumpScale import j
-
+import netaddr
 import VXNet.vxlan as vxlan
 from netaddr import *
 import VXNet.netclasses as netcl
@@ -21,6 +21,34 @@ class NetConfigFactory():
             self._layout.load()
         # add_ips_to(self._layout)  #@todo fix
         return self._layout.nicdetail
+
+    def _exec(self,cmd,failOnError=True):
+        print cmd
+        rc,out=j.system.process.execute(cmd,dieOnNonZeroExitCode=failOnError)        
+        return out
+        
+
+    def removeOldConfig(self):
+        cmd="brctl show"
+        for line in self._exec(cmd).split("\n"):
+            if line.strip()=="" or line.find("bridge name")<>-1:
+                continue
+            name=line.split("\t")[0]
+            self._exec("ip link set %s down"%name)
+            self._exec("brctl delbr %s"%name)
+
+        self.getConfigFromSystem(reload=True)
+
+        for intname,data in self.getConfigFromSystem().iteritems():
+            if "PHYS" in data["detail"]:
+                self._exec("ip link set %s down"%intname,False)
+            if intname =="ovs-system":
+                continue
+            self._exec("ovs-vsctl del-br %s"%intname,False)
+
+        self.getConfigFromSystem(reload=True)
+
+        j.system.fs.writeFile(filename="/etc/network/interfaces",contents="auto lo\n iface lo inet loopback\n\n")
 
     def printConfigFromSystem(self):
         pprint_dict(self.getConfigFromSystem())
@@ -55,9 +83,68 @@ class NetConfigFactory():
             return interf["params"]["type"]
         return None
         
+    def setBackplaneDhcp(self,interfacename="eth0",backplaneId=1):
+        """
+        DANGEROUS, will remove old configuration
+        """
+        C="""
+auto Backplane$id
+allow-ovs Backplane1
+iface Backplane$id inet dhcp
+ dns-nameserver 8.8.8.8 8.8.4.4
+ ovs_type OVSBridge
+ ovs_ports $iname
 
+allow-Backplane$id $iname
+iface $iname inet manual
+ ovs_bridge Backplane1
+ ovs_type OVSPort
+"""
+        C=C.replace("$id", str(backplaneId))
+        C=C.replace("$iname", interfacename)
 
+        ed=j.codetools.getTextFileEditor("/etc/network/interfaces")
+        ed.setSection(interfacename,C)
 
+    def setBackplane(self,interfacename="eth0",backplaneId=1,ipaddr="192.168.10.10/24",gw=""):
+        """
+        DANGEROUS, will remove old configuration
+        """
+        C="""
+auto Backplane$id
+allow-ovs Backplane1
+iface Backplane$id inet static
+ address $ipbase 
+ netmask $mask
+ dns-nameserver 8.8.8.8 8.8.4.4
+ ovs_type OVSBridge
+ ovs_ports $iname
+ $gw
+
+allow-Backplane$id $iname
+iface $iname inet manual
+ ovs_bridge Backplane1
+ ovs_type OVSPort
+"""
+        n=netaddr.IPNetwork(ipaddr)
+
+        C=C.replace("$id", str(backplaneId))
+        C=C.replace("$iname", interfacename)
+        C=C.replace("$ipbase", str(n.ip))
+        C=C.replace("$mask", str(n.netmask))
+        if gw<>"" and gw<>None:
+            C=C.replace("$gw", "gateway %s"%gw)
+        else:
+            C=C.replace("$gw", "")
+
+        ed=j.codetools.getTextFileEditor("/etc/network/interfaces")
+        ed.setSection(interfacename,C)
+        ed.save()
+
+        self._exec("ifdown Backplane%s"%backplaneId, failOnError=False)
+        self._exec("ifup Backplane%s"%backplaneId, failOnError=True)
+
+        print self._exec("ovs-vsctl show", failOnError=True)
 
 
     
