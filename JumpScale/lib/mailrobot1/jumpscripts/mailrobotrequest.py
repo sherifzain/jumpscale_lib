e4cac7bdddfdd5277328d4ddb3e51838f1eeda56
Sherif Zain <sherifzain@yahoo.com>
2014-06-18 12:33:06 +0200

    Fixed: types errors

--- JumpScale/lib/mailrobot1/jumpscripts/mailrobotrequest.py
+++ JumpScale/lib/mailrobot1/jumpscripts/mailrobotrequest.py
@@ -15,7 +15,7 @@ async = True
 def action(appkwargs, hrd):
     import JumpScale.lib.ms1
     j.tools.ms1.setSecret(appkwargs['appdeck.secret'], True)
-    j.tools.ms1.deployAppDeck(appkwargs['appdeck.location'], appkwargs['appdeck.app.name'], appkwargs['appdeck.app.memsize'],
-                              appkwargs['appdeck.app.ssdsize'], appkwargs['appdeck.app.vsansize'], appkwargs['appdeck.app.jpdomain'],
+    j.tools.ms1.deployAppDeck(appkwargs['appdeck.location'], appkwargs['appdeck.app.name'], int(appkwargs['appdeck.app.memsize']),
+                              int(appkwargs['appdeck.app.ssdsize']), int(appkwargs['appdeck.app.vsansize']), appkwargs['appdeck.app.jpdomain'],
                               appkwargs['appdeck.app.jpname'], config=hrd, description=appkwargs['appdeck.app.description'])
     return True
