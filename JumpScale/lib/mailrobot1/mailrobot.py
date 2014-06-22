32c94c428eb7fa57d6e58af7f1b675dce8321076
Sherif Zain <sherifzain@yahoo.com>
2014-06-22 15:06:32 +0200

    WIP: reworking mail server using robots factory

--- JumpScale/lib/mailrobot1/mailrobot.py
+++ JumpScale/lib/mailrobot1/mailrobot.py
@@ -40,52 +40,26 @@ class MailRobot(SMTPServer):
                 msg_parts = msg.get_payload()
                 ct2msg = dict([(msg_part.get_content_type(), msg_part) for msg_part in msg_parts])
                 if 'text/plain' in ct2msg:
-                    hrdstr = ct2msg['text/plain'].get_payload()
+                    commands_str = ct2msg['text/plain'].get_payload()
                 else:
                     for ct, msg_part in ct2msg.iteritems():
                         if 'text' in ct:
-                            hrdstr = self._html2text(msg_part.get_payload())
+                            commands_str = self._html2text(msg_part.get_payload())
                             break
             else:
-                hrdstr = msg.get_payload()
-            try:
-                hrd = j.core.hrd.getHRD(content=hrdstr)
-            except:
-                print "Invalid hrd given %s" % data
-                raise
+                commands_str = msg.get_payload()
+
             print "Processing message from %s"  % msg['From']
-            allkeys = hrd.prefix('')
-            appdict = dict()
-            hrddict = dict()
-            for key in allkeys:
-                if key.startswith('_') or key in ('changed', 'path'):
-                    continue
-                if key.startswith('appdeck'):
-                    appdict[key] = hrd.get(key)
-                hrddict[key] = hrd.get(key)
-            hrd_config = ""
-            for line in hrdstr.splitlines():
-                if line.startswith('appdeck'):
-                    continue
-                hrd_config += line + "\n"
-            result = self.acl.executeJumpScript('jumpscale', 'mailrobotrequest', nid=j.application.whoAmI.nid, args={'appkwargs': appdict, 'hrd': hrd_config})
-            appname = appdict.get("appdeck.app.name", "default")
-            if result['state'] != "OK":
-                msg = """
-You request for deployment has failed.
-Our support team has been notified and will contact you as soon as possible
-    """
-                j.clients.email.send([mailfrom], 'mailrobot@mothership1.com', 'Deployment %s Failed' % appname, msg)
-            else:
-                template = j.system.fs.fileGetContents(j.system.fs.joinPaths(self.templatefolder, "%s.tmpl" % appname))
-                msg = hrd.applyOnContent(template, result['result'])
-                j.clients.email.send([mailfrom], 'mailrobot@mothership1.com', 'Deployment %s Succesfull' % appname, msg)
+            output = ''
+            if msg['To'].split('@')[0] == 'youtrack':
+                import JumpScale.lib.youtrackclient
+                robot = j.tools.youtrack.getRobot("http://incubaid.myjetbrains.com/youtrack/")
+                output = robot.process(commands_str)
+            
+            output = output or 'A generic error has occured. Please try again later'
+            j.clients.email.send([mailfrom], 'mailrobot@mothership1.com', msg.get('subject'), output)
         except:
-            msg = '''
-Deployment failed. A generic error has occured.
-Please try again later.
-    '''
-            j.clients.email.send([mailfrom], 'mailrobot@mothership1.com', 'Deployment Failed', msg)
+            j.clients.email.send([mailfrom], 'mailrobot@mothership1.com', msg.get('subject'), 'A generic error has occured. Please try again later')
 
 class MailRobotFactory(object):
     def start(self):
